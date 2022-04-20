6ed6a287688d0c38e1e876c7b44863f0a7d42003
Alberto Basalo <albertobasalo@hotmail.com>
2022-04-20 16:31:35 +0200

    feat: error

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,10 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { Agency } from '@stk/models/agency.interface';
 import { catchError, map, Observable, of } from 'rxjs';
 import { HomeService } from './home.service';
 
-type Api = {
-  data?: unknown[];
+type Api<T> = {
+  data?: T[];
   error?: string;
 };
 
@@ -14,7 +15,7 @@ type Api = {
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HomeComponent implements OnInit {
-  agencies$: Observable<Api> = this.service.getAgencies$().pipe(
+  agencies$: Observable<Api<Agency>> = this.service.getAgencies$().pipe(
     map((data) => ({ data: data, error: undefined })),
     catchError((error) => of({ data: undefined, error: error.message }))
   );
@@ -30,7 +31,7 @@ export class HomeComponent implements OnInit {
   ngOnInit(): void {}
 
   loadAgencies() {
-    this.agencies$ = this.service.getAgencies$();
+    // this.agencies$ = this.service.getAgencies$();
   }
   loadTrips() {
     this.trips$ = this.service.getTrips$();

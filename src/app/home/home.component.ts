243a785abeecaff38c8bbf672635d59262634d8f
Alberto Basalo <albertobasalo@hotmail.com>
2022-04-20 18:55:02 +0200

    feat: clean

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,7 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { Agency } from '@stk/models/agency.interface';
 import { Trip } from '@stk/models/trip.interface';
-import { catchError, map, Observable, of } from 'rxjs';
+import { Observable } from 'rxjs';
 import { HomeService } from './home.service';
 
 type Api<T> = {
@@ -17,11 +17,7 @@ type Api<T> = {
 })
 export class HomeComponent implements OnInit {
   agencies$: Observable<Agency[]> = this.service.getAgencies$();
-
-  trips$: Observable<Api<Trip>> = this.service.getTrips$().pipe(
-    map((data) => ({ data: data, error: undefined })),
-    catchError((error) => of({ data: undefined, error: error.message }))
-  );
+  trips$: Observable<Trip[]> = this.service.getTrips$();
 
   constructor(private service: HomeService) {}
 
@@ -31,9 +27,6 @@ export class HomeComponent implements OnInit {
     this.agencies$ = this.service.getAgencies$();
   }
   loadTrips() {
-    this.trips$ = this.service.getTrips$().pipe(
-      map((data) => ({ data })),
-      catchError((error) => of({ error: error.message }))
-    );
+    this.trips$ = this.service.getTrips$();
   }
 }

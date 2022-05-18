59782cdf39815629f97f74d3acf8041630e92b18
Alberto Basalo <albertobasalo@hotmail.com>
2022-05-18 16:34:39 +0200

    feat: redux

--- src/app/core/atomic.store.ts
+++ src/app/core/atomic.store.ts
@@ -1,18 +1,25 @@
-import { BehaviorSubject, map, Observable, ReplaySubject } from 'rxjs';
+import { BehaviorSubject, distinctUntilChanged, map, Observable, ReplaySubject } from 'rxjs';
 
 export type Action = { type: string; payload: unknown };
 export type Change<T> = { action: Action; current: T; next: T };
-
-// export type Reducer<T> = (state: T, payload: unknown) => T;
-// export type Selector<T, K> = (state: T) => K;
+export type Reducer<T> = (state: T, payload: any) => T;
+export type Selector<T, K> = (state: T) => K;
 
 export class AtomicStore<T> {
   private state$: BehaviorSubject<T>;
   private changes$: ReplaySubject<Change<T>>;
 
+  public reducers: Map<string, Reducer<T>> = new Map();
+
+  private setStateReducer: Reducer<T> = (state, payload) => ({
+    ...state,
+    ...(payload as Partial<T>),
+  });
+
   constructor(initialValue: T) {
     this.state$ = new BehaviorSubject(this.clone(initialValue));
     this.changes$ = new ReplaySubject();
+    this.reducers.set('SetState', this.setStateReducer);
   }
 
   set(payload: Partial<T>): void {
@@ -20,13 +27,12 @@ export class AtomicStore<T> {
   }
 
   dispatch(action: Action): void {
-    const payload = action.payload as Partial<T>;
-    if (payload) {
-      const current = this.get();
-      const next = { ...current, ...payload }; // reducing
-      this.state$.next(next);
-      this.changes$.next({ action, current, next: this.clone(next) });
-    }
+    const reducer = this.reducers.get(action.type);
+    if (reducer == null) throw Error('No reducer found for action type ' + action.type);
+    const current = this.get();
+    const next = reducer(current, action.payload);
+    this.state$.next(next);
+    this.changes$.next({ action, current, next: this.clone(next) });
   }
 
   get(): T {
@@ -35,6 +41,14 @@ export class AtomicStore<T> {
 
   get$(): Observable<T> {
     return this.state$.asObservable().pipe(map((state) => this.clone(state)));
+  }
+
+  select$<K>(selector: Selector<T, K>): Observable<K> {
+    return this.get$().pipe(map(selector), distinctUntilChanged());
+  }
+
+  getChanges$(): Observable<Change<T>> {
+    return this.changes$.asObservable().pipe(map((change) => ({ ...change })));
   }
 
   private clone(state: T): T {
--- src/app/core/cuenta.service.ts
+++ src/app/core/cuenta.service.ts
@@ -21,4 +21,41 @@ cuentaStore.set({ IBAN: '65465', balance: 6000, owners: [] });
 cuentaStore.get$().subscribe((cuenta) => console.log(cuenta.balance));
 console.log(cuentaStore.get()); // 6000
 
+cuentaStore.select$<string>((cuenta) => cuenta.IBAN);
+
 // cuentaStore.get$().next();
+
+//(state as any as Cuenta).balance / (state as any as Cuenta).owners.length
+
+function ingresar(current: Cuenta, importe: number): Cuenta {
+  const state = { ...current };
+  state.balance += importe;
+  return state;
+}
+function retirar(current: Cuenta, importe: number): Cuenta {
+  const state = { ...current };
+  state.balance -= importe;
+  return state;
+}
+function agregarTitular(current: Cuenta, titular: string): Cuenta {
+  const state = { ...current };
+  state.owners.push(titular);
+  return state;
+}
+
+//const cotizaciones = [];
+
+function ingresarBitcoinsReducer(
+  current: Cuenta,
+  payload: { bitcoins: number; cotización: number }
+): Cuenta {
+  const state = { ...current };
+  const importe = payload.bitcoins * payload.cotización;
+  state.balance += importe;
+  return state;
+}
+
+cuentaStore.reducers.set('Ingresar', ingresar);
+cuentaStore.reducers.set('IngresarBitcoin', ingresarBitcoinsReducer);
+
+cuentaStore.dispatch({ type: 'IngresarBitcoin', payload: { bitcoins: 0.005, cotización: 28000 } });
